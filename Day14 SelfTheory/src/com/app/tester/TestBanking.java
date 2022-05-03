601fc453f6dfe8eaa5739fea2bbe9181442809a6
Hardik Agarwal <hardikagarwal1703@gmail.com>
2022-05-03 13:47:22 +0530

    [Hardik] Added all cases of 03-05-2022

--- Day14 SelfTheory/src/com/app/tester/TestBanking.java
+++ Day14 SelfTheory/src/com/app/tester/TestBanking.java
@@ -23,6 +23,8 @@ public class TestBanking {
 				System.out.println("Enter Choice:");
 				System.out.println("1. Create A/c");
 				System.out.println("2. Display All A/c");
+				System.out.println("3. A/c Summary");
+				System.out.println("4. Fund Transfer");
 				System.out.println("10. Exit");
 
 				try{
@@ -30,23 +32,70 @@ public class TestBanking {
 						case 1:
 								System.out.println("Enter A/c No:");
 								int accNo = Integer.parseInt(scan.nextLine());
+
+								//containsKey will look for already existing or not 
+								//avoiding duplicates
 								if(accountMap.containsKey(accNo)) {
 									throw new BankingException("Account Already Exist");
 								}
+
 								System.out.println("Enter A/c Details: customerName, accType, "
 										+ "creationDate(yyyy-MM-dd), balance");
 								accountMap.put(accNo, new BankAccount(accNo, scan.nextLine(), 
 										AcType.valueOf(scan.nextLine().toUpperCase()), 
 										LocalDate.parse(scan.nextLine()),
 										BankingValidationRules.validateBalance(Double.parseDouble(scan.nextLine()))));
-								System.out.println("A/c Added");
+
+								System.out.println("A/c Added with a/c no: " + accNo);
 								break;
-						case 2:
-								System.out.println();
+
+						case 2: 
+								System.out.println("All a/c in Bank:");
+								for(BankAccount a: accountMap.values()) {
+									System.out.println(a);
+								}
 								break;
+
+						case 3:
+								System.out.println("Enter A/c no:");
+
+								//this method will directly get the value based on i/p key
+								BankAccount acc = accountMap.get(Integer.parseInt(scan.nextLine()));
+								if(acc == null) {
+									throw new BankingException("Invalid a/c No!!!");
+								}
+								System.out.println("Bank A/c Summary: " + acc);
+								break;
+
+						case 4:
+								System.out.println("Enter Source & Dest A/c No:");
+								acc = accountMap.get(Integer.parseInt(scan.nextLine()));
+								if(acc == null) {
+									throw new BankingException("Invalid source a/c No!!!");
+								}
+								BankAccount destAcc = accountMap.get(Integer.parseInt(scan.nextLine()));
+								if(destAcc == null) {
+									throw new BankingException("Invalid dest a/c No!!!");
+								}
+								System.out.println("Enter Transfer Amount");
+								acc.fundsTransfer(destAcc, Double.parseDouble(scan.nextLine()));
+								break;
+
+						case 5:
+								System.out.println("Enter A/c no to close");
+
+								//this method removes the key-value pair with passed key.
+								acc = accountMap.remove(Integer.parseInt(scan.nextLine()));
+								if(acc == null) {
+									throw new BankingException("Invalid a/c No! Can't close a/c.");
+								}
+								System.out.println("Closed A/c: " + acc);
+								break;
+
 						case 10:
 								exit = true;
 								break;
+
 					}
 				}catch(Exception e) {
 					e.printStackTrace();
